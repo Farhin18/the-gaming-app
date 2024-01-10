ee3c7852c65695da38c0c519392714ce950acc08
FarhinMirza <farhinmirza93414@gmail.com>
2024-01-10 13:53:24 +0530

    adding palette

--- src/Login.js
+++ src/Login.js
@@ -12,16 +12,17 @@ export const Login = (props) => {
     return(
         <div className="auth-form-container">
             <h2>Login</h2>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="email">email</label>
                 <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"/><br/>
                 <label htmlFor="password">password</label>
                     <input value={pass} type="password" placeholder="******" id="password" name="password"/><br/>
                     <button type="submit">
-                        Log In
+                        sign in
                     </button>
                     </form>
-                    <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+                    
         </div>
     )
 }
---
+++ src/style/color.js
@@ -0,0 +1,16 @@
+export const Palette = {
+  Red: {
+    Light: "#D9083A",
+    Dark: "#8C0327",
+  },
+
+  Blue: {
+    Dark: "#041656",
+    Light: "#154485",
+  },
+
+  Gold: {
+    Light: "#F2CF63",
+    Dark: "#D5A045",
+  },
+};
